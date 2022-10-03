f88b3ec83e8a3c36697b6c41b13a67e2feb9bd6e
Albin Hagström <albin.hagstrom@nentgroup.com>
2022-10-03 15:53:57 +0200

    favicon bs

--- src/main.ts
+++ src/main.ts
@@ -16,3 +16,12 @@ world.onReady = () => {
 loadingScreen.addEventListener('onstart', () => {
     world.start();
 });
+
+const favicon = document.querySelector('link[rel="icon"]')
+let i = 0;
+
+setInterval(() => {
+    favicon?.setAttribute('href', `/fav${i}.svg`);
+    if (i) i = 0;
+    else i = 1;
+}, 1000);
